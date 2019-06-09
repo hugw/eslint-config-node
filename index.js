d16c8f768e9c37e1db80a0e9c074357061b024c8
Hugo Wantuil <hugw@users.noreply.github.com>
2019-06-09 09:58:54 -0400

    Add explicit alias to babel-module settings

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ module.exports = {
   },
   settings: {
     'import/resolver': {
-      'babel-module': {}
+      'babel-module': {
+        'alias': {
+          '@': './src'
+        }
+      }
     }
   }
 }
